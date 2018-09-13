3d4d1f579533bd26ef15f4da8adfa8e01d86b3b7
Sphelele Myeni <siphelele@thedigitalacademy.co.za>
2018-09-13 16:27:18 +0200

    Edited splash animation

--- app/src/main/java/com/example/academy_intern/stric_code/MainActivity.java
+++ app/src/main/java/com/example/academy_intern/stric_code/MainActivity.java
@@ -3,26 +3,32 @@ package com.example.academy_intern.stric_code;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.example.academy_intern.stric_code.fragment.fragmentLogin;
-import com.example.academy_intern.stric_code.fragment.fragmentSplash;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView zoom;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new CountDownTimer(5000, 1000) { //5 seconds
+        new CountDownTimer(2000, 1000) { //5 seconds
             public void onTick(long millisUntilFinished) {
 
-                getFragmentManager().beginTransaction().add(R.id.mainId , new fragmentSplash()).commit();
+                zoom =  (ImageView) findViewById(R.id.imageView);
+                final Animation zoomAnimation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.zoom);
+                zoom.startAnimation(zoomAnimation);
 
             }
 
             public void onFinish() {
+                zoom.setVisibility(View.GONE);
                 getFragmentManager().beginTransaction().replace(R.id.mainId , new fragmentLogin()).commit();
             }
 
